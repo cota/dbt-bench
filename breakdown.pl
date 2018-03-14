f3a7eb0c02dc47ee5bef4f8c173d2437c8e6309b
Emilio G. Cota <cota@braap.org>
2018-03-14 19:58:20 -0400

    perl scripts: find Mean.pm regardless of cwd
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- breakdown.pl
+++ breakdown.pl
@@ -3,6 +3,8 @@
 
 use warnings;
 use strict;
+use File::Basename;
+use lib dirname (__FILE__);
 use Mean;
 use Getopt::Long;
 
--- dat-perl.pl
+++ dat-perl.pl
@@ -2,6 +2,8 @@
 
 use warnings;
 use strict;
+use File::Basename;
+use lib dirname (__FILE__);
 use Mean;
 
 my @files = @ARGV;
